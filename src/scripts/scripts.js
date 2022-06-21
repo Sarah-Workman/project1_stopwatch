219b61d974df1d6e231b6943a75a73df365607a8
sarah-workman <swork623@gmail.com>
2022-06-21 15:20:43 -0500

    buttons in and wired to functions.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,1 +1,8 @@
 /* Your JavaScript here */
+document.getElementById("start").addEventListener("click", start);
+document.getElementById("pause").addEventListener("click", pause);
+document.getElementById("stop").addEventListener("click", stop);
+
+function start() {}
+function pause() {}
+function stop() {}
