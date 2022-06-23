110bea6afe5617f917a3807b33389ce827de8c9b
sarah-workman <swork623@gmail.com>
2022-06-23 01:16:23 -0500

    start function update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,9 @@
 let seconds = document.getElementById("seconds");
 let minutes = document.getElementById("minutes");
 let hours = document.getElementById("hours");
-let outputseconds = null;
+let outputseconds;
+let outputminutes;
+let outputhours;
 
 //connection to button
 document.getElementById("start").addEventListener("click", start);
@@ -14,12 +16,28 @@ function start() {
 	startTimer = setInterval(function () {
 		seconds++;
 		if (seconds < 09) {
-			outputseconds = 0 + seconds;
+			outputseconds = "0" + seconds;
 			document.getElementById("seconds").innerHTML = outputseconds;
-		}
-		if (seconds < 99) {
+		} else if (seconds < 99) {
+			outputseconds = seconds;
+			document.getElementById("seconds").innerHTML = outputseconds;
+		} else if (seconds > 99) {
+			minutes++;
+			outputminutes = 0 + minutes;
+			document.getElementById("minutes").innerHTML = outputminutes;
+		} else if (minutes > 9) {
+			outputminutes = minutes;
+			document.getElementById("minutes").innerHTML = outputminutes;
+		} else if (minutes > 99) {
+			hours++;
+			outputhours = 0 + hours;
+			document.getElementById("hours").innerHTML = outputhours;
+		} else if (hours > 9) {
+			outputhours = hours;
+			document.getElementById("hours").innderHTML = outputhours;
 		}
 	}, 1000);
-}
+} //end of start function
+
 function pause() {}
 function stop() {}
