33a9a8b005760be3fd815066fa1eb4a8f2144121
sarah-workman <swork623@gmail.com>
2022-06-23 00:56:44 -0500

    update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,8 +1,25 @@
-/* Your JavaScript here */
+let seconds = document.getElementById("seconds");
+let minutes = document.getElementById("minutes");
+let hours = document.getElementById("hours");
+let outputseconds = null;
+
+//connection to button
 document.getElementById("start").addEventListener("click", start);
 document.getElementById("pause").addEventListener("click", pause);
 document.getElementById("stop").addEventListener("click", stop);
 
-function start() {}
+//functions
+function start() {
+	//interval
+	startTimer = setInterval(function () {
+		seconds++;
+		if (seconds < 09) {
+			outputseconds = 0 + seconds;
+			document.getElementById("seconds").innerHTML = outputseconds;
+		}
+		if (seconds < 99) {
+		}
+	}, 1000);
+}
 function pause() {}
 function stop() {}
