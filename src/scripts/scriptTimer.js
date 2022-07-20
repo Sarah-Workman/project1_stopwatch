e3ef526b64c1b061de22e342d36b7d6f364a14f4
sarah-workman <swork623@gmail.com>
2022-07-20 18:37:12 -0500

    timer function changes

--- src/scripts/scriptTimer.js
+++ src/scripts/scriptTimer.js
@@ -26,65 +26,49 @@ document.getElementById("formBtn").addEventListener("click", function () {
 	minutes = document.getElementById("timerMinutes").value;
 	seconds = document.getElementById("timerSeconds").value;
 
-	alert("hours is " + hours);
-
 	// the start of the main timer function. I planned on doing the opposite of the stopwatch using the passed value as the starting point for countdown.
 	if (statusCheck === 0) {
 		statusCheck = 1;
 
 		//interval
 		timerInterval = setInterval(function () {
-			seconds--;
-			if (seconds <= 9 && seconds >= 0) {
+			if (seconds < 60 && seconds > 0) {
+				seconds--;
 				// put seconds on screen with 0 place holder
-				outputseconds = "0" + seconds;
+				outputseconds = seconds;
 				document.getElementById("inputSeconds").innerHTML = outputseconds;
 
 				// static minutes and hours
 				document.getElementById("inputMinutes").innerHTML = minutes;
 				document.getElementById("inputHours").innerHTML = hours;
+			} //check to see if minute is over
+			else if (minutes < 60 && minutes > 0) {
+				minutes--;
 
-				//check to see if minute is over
 				if (seconds === 0) {
-					minutes--;
 					seconds = 59;
-					if (minutes <= 9 && minutes >= 0) {
-						outputminutes = "0" + minutes;
+					outputminutes = minutes;
 
-						document.getElementById("inputMinutes").innerHTML = outputminutes;
-						document.getElementById("inputSeconds").innerHTML = seconds;
+					document.getElementById("inputMinutes").innerHTML = outputminutes;
+					document.getElementById("inputSeconds").innerHTML = seconds;
 
-						// to check if the hour is over
-					}
-				} else if (minutes === 0) {
-					hours--;
-					minutes = 59;
-					if (hours <= 9 && hours >= 0) {
-						outputhours = "0" + hours;
-						outputminutes = minutes;
-						outputseconds = seconds;
-						document.getElementById("inputhours").innerHTML = outputhours;
-						document.getElementById("inputMinutes").innerHTML = outputseconds;
-						document.getElementById("inputSeconds").innerHTML = outputseconds;
-					}
+					// to check if the hour is over
 				}
-			} else if (seconds >= 9 && seconds <= 60) {
-				outputseconds = seconds;
-				document.getElementById("inputSeconds").innerHTML = outputseconds;
-				// just put minutes on screen as a static untill..
-				document.getElementById("inputMinutes").innerHTML = minutes;
-				//check to see if minute is over
-				if (seconds === 0) {
-					minutes--;
-					seconds = 59;
-					if (minutes <= 9 && minutes >= 0) {
-						outputminutes = "0" + minutes;
-						outputseconds = seconds;
-						document.getElementById("inputMinutes").innerHTML = outputminutes;
-						document.getElementById("inputSeconds").innerHTML = outputseconds;
-					}
+			} else if (hours < 60 && hours > 0) {
+				hours--;
+
+				if (minutes === 0) {
+					minutes = 59;
+					outputhours = hours;
+					outputminutes = minutes;
+					outputseconds = seconds;
+					document.getElementById("inputhours").innerHTML = outputhours;
+					document.getElementById("inputMinutes").innerHTML = outputseconds;
+					document.getElementById("inputSeconds").innerHTML = outputseconds;
 				}
 			}
+
+			//redex to see if two digits?
 		}, 1000); //empty function
 	} //end of first if check
 }); //end of function
