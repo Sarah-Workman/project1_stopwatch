7f028e97c6a9c81037a316a2e6b16c9ff67a7fa4
sarah-workman <swork623@gmail.com>
2022-07-22 11:06:03 -0500

    added pause functionality. Still working on 0.

--- src/scripts/scriptTimer.js
+++ src/scripts/scriptTimer.js
@@ -29,44 +29,45 @@ document.getElementById("formBtn").addEventListener("click", function () {
 	// the start of the main timer function. I planned on doing the opposite of the stopwatch using the passed value as the starting point for countdown.
 	if (statusCheck === 0) {
 		statusCheck = 1;
+		let seclength = seconds.toString.length;
+		let minlength = minutes.toString.length;
+		let hourlength = hours.toString.length;
 
 		//interval
 		timerInterval = setInterval(function () {
 			if (seconds < 60 && seconds > 0) {
 				document.getElementById("inputSeconds").innerHTML = seconds;
-				seconds--;
-
-				document.getElementById("inputSeconds").innerHTML = seconds;
 				document.getElementById("inputMinutes").innerHTML = minutes;
 				document.getElementById("inputHours").innerHTML = hours;
-				if (seconds === 0 && minutes === 0 && hours === 0) {
-					seconds = "00";
-					minutes = "00";
-					hours = "00";
-					document.getElementById("inputSeconds").innerHTML = seconds;
-					document.getElementById("inputMinutes").innerHTML = minutes;
-					document.getElementById("inputHours").innerHTML = hours;
-				}
+				seconds--;
 			} else if (minutes < 60 && minutes > 0) {
 				document.getElementById("inputMinutes").innerHTML = minutes;
+				document.getElementById("inputSeconds").innerHTML = seconds;
+				document.getElementById("inputHours").innerHTML = hours;
 				minutes--;
 				seconds = 59;
 			} else if (hours < 60 && hours > 0) {
 				document.getElementById("inputHours").innerHTML = hours;
+				document.getElementById("inputMinutes").innerHTML = minutes;
+				document.getElementById("inputSeconds").innerHTML = seconds;
 				hours--;
 				minutes = 59;
 			}
 
 			//redex to see if two digits?
 		}, 1000); //empty function
+		outputhours = hours;
+		outputminutes = minutes;
+		outputseconds = seconds;
+		return hours + minutes + seconds;
 	} //end of first if check
 }); //end of function
 
-document.getElementById("startTimer").addEventListener(click, function () {}); //end of function
+document.getElementById("startTimer").addEventListener("click", function () {}); //end of function
 
-document.getElementById("pauseTimer").addEventListener(click, function () {
+document.getElementById("pauseTimer").addEventListener("click", function () {
 	clearInterval(timerInterval);
 	document.getElementById("inputSeconds").innerHTML = seconds;
 	document.getElementById("inputMinutes").innerHTML = minutes;
-	document.getElementById("inputHours").innderHTML = hours;
+	document.getElementById("inputHours").innerHTML = hours;
 }); //end of function
