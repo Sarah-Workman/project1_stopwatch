9caa1dc112e0a85472dacf438c5325ca7684e1c7
sarah-workman <swork623@gmail.com>
2022-08-01 10:11:58 -0500

    placeholder bug almost fixed

--- src/scripts/scriptTimer.js
+++ src/scripts/scriptTimer.js
@@ -2,14 +2,15 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let timerInterval = null;
-let time;
-let statusCheck = 0;
-let outputseconds;
-let outputminutes;
-let outputhours;
+let time = [];
+
+let isRunning = false;
+let outputseconds = "";
+let outputminutes = "";
+let outputhours = "";
 
 //form is opened
-document.getElementById("addTimer").addEventListener("click", function () {
+document.getElementById("addTimer").addEventListener("click", () => {
 	let inputForm = document.getElementById("timerFormSec");
 	$(document).ready(function () {
 		$(inputForm).css("display", "block");
@@ -17,7 +18,7 @@ document.getElementById("addTimer").addEventListener("click", function () {
 });
 
 //form btn is pressed and values are grabed. hide and start function start with values returned.
-document.getElementById("formBtn").addEventListener("click", function () {
+document.getElementById("formBtn").addEventListener("click", () => {
 	hours = document.getElementById("timerHours").value;
 	minutes = document.getElementById("timerMinutes").value;
 	seconds = document.getElementById("timerSeconds").value;
@@ -34,33 +35,83 @@ function hide() {
 
 //starts the main timer function. Opposite of the stopwatch using the passed value as the starting point for countdown.
 function start() {
-	if (statusCheck === 0) {
-		statusCheck = 1;
-		let seclength = seconds.toString.length;
-		let minlength = minutes.toString.length;
-		let hourlength = hours.toString.length;
-
-		let zeroDig = 0;
-		let oneDig = 1;
-		let twoDig = 2;
+	if (isRunning === false) {
+		statusCheck = true;
 
 		//interval
-		timerInterval = setInterval(function () {
+		timerInterval = setInterval(() => {
 			if (seconds < 60 && seconds > 0) {
-				document.getElementById("inputSeconds").innerHTML = seconds;
-				document.getElementById("inputMinutes").innerHTML = minutes;
-				document.getElementById("inputHours").innerHTML = hours;
+				// if the length is less then two add a zero in from of value
+				for (let a = seconds.toString.length; a < 2; a++) {
+					outputseconds = "0" + seconds;
+
+					for (let b = minutes.toString.length; b < 2; b++) {
+						outputminutes = "0" + minutes;
+
+						for (let c = hours.toString.length; c < 2; c++) {
+							outputhours = "0" + hours;
+						}
+					}
+				}
+				//remove any extra zeros
+				if (outputseconds.length > 2) {
+					outputseconds = outputseconds.replace("0", "");
+					if (outputminutes.length > 2) {
+						outputminutes = outputminutes.replace("0", "");
+					}
+				}
+				//display time and main function of timer
+				document.getElementById("inputSeconds").innerHTML = outputseconds;
+				document.getElementById("inputMinutes").innerHTML = outputminutes;
+				document.getElementById("inputHours").innerHTML = outputhours;
 				seconds--;
 			} else if (minutes < 60 && minutes > 0) {
-				document.getElementById("inputMinutes").innerHTML = minutes;
-				document.getElementById("inputSeconds").innerHTML = seconds;
-				document.getElementById("inputHours").innerHTML = hours;
+				// if the length is less then two add a zero in from of value
+				for (let a = seconds.toString.length; a < "2"; a++) {
+					console.log("a is: " + a);
+					outputseconds = 0 + seconds;
+
+					for (let b = minutes.toString.length; b < "2"; b++) {
+						console.log("b is: " + b);
+						outputminutes = 0 + minutes;
+
+						for (let c = hours.toString.length; c < "2"; c++) {
+							console.log("c is: " + c);
+							outputhours = 0 + hours;
+						}
+					}
+				}
+				//remove any extra zeros
+				if (outputminutes.length > 2) {
+					outputminutes = outputminutes.replace("0", "");
+				}
+
+				//display time and continue main function of timer
+				document.getElementById("inputMinutes").innerHTML = outputminutes;
+				document.getElementById("inputSeconds").innerHTML = outputseconds;
+				document.getElementById("inputHours").innerHTML = outputhours;
 				minutes--;
 				seconds = 59;
 			} else if (hours < 60 && hours > 0) {
-				document.getElementById("inputHours").innerHTML = hours;
-				document.getElementById("inputMinutes").innerHTML = minutes;
-				document.getElementById("inputSeconds").innerHTML = seconds;
+				// if the length is less then two add a zero in from of value
+				for (let a = seconds.toString.length; a < "2"; a++) {
+					outputseconds = 0 + seconds;
+
+					for (let b = minutes.toString.length; b < "2"; b++) {
+						outputminutes = 0 + minutes;
+
+						for (let c = hours.toString.length; c < "2"; c++) {
+							outputhours = 0 + hours;
+						}
+					}
+				}
+				//display time and continue main function of timer
+				if (outputminutes.length > 2) {
+					outputminutes = outputminutes.replace("0", "");
+				}
+				document.getElementById("inputHours").innerHTML = outputhours;
+				document.getElementById("inputMinutes").innerHTML = outputminutes;
+				document.getElementById("inputSeconds").innerHTML = outputhours;
 				hours--;
 				minutes = 59;
 			}
@@ -70,27 +121,29 @@ function start() {
 
 		// return values to pass to pause timer
 
-		return hours + minutes + seconds;
+		return (
+			seconds + minutes + hours + outputseconds + outputminutes + outputhours
+		);
 	} //end of first if check
 } //end of function
 
-document.getElementById("startTimer").addEventListener("click", function () {
+document.getElementById("startTimer").addEventListener("click", () => {
 	clearInterval(timerInterval);
-	statusCheck = 0;
+	statusCheck = false;
 	start();
 }); //end of function
 
-document.getElementById("pauseTimer").addEventListener("click", function () {
+document.getElementById("pauseTimer").addEventListener("click", () => {
 	clearInterval(timerInterval);
-	statusCheck = 0;
-	document.getElementById("inputSeconds").innerHTML = seconds;
-	document.getElementById("inputMinutes").innerHTML = minutes;
-	document.getElementById("inputHours").innerHTML = hours;
+	statusCheck = false;
+	document.getElementById("inputSeconds").innerHTML = outputseconds;
+	document.getElementById("inputMinutes").innerHTML = outputminutes;
+	document.getElementById("inputHours").innerHTML = outputhours;
 }); //end of function
 
-document.getElementById("resetTimer").addEventListener("click", function () {
+document.getElementById("resetTimer").addEventListener("click", () => {
 	let startTimer = "00";
-	statusCheck = 0;
+	statusCheck = false;
 	clearInterval(timerInterval);
 	document.getElementById("inputSeconds").innerHTML = startTimer;
 	document.getElementById("inputMinutes").innerHTML = startTimer;
